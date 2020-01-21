598bf875906361df9bd9cb3057785a7848ca1318
Taulant Culi <t.culi91@gmail.com>
2020-01-21 17:41:46 +0100

    Updates

---
+++ precache-manifest.a54632ea8a9512aed8c66f5788872940.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81b3953ef3b2435217242b9676b3cbec",
+    "url": "/react-project2/tree/master/react-app/index.html"
+  },
+  {
+    "revision": "b5763d792ba47c2594e5",
+    "url": "/react-project2/tree/master/react-app/static/css/main.a0cc64f8.chunk.css"
+  },
+  {
+    "revision": "502525a3491ad0eaae17",
+    "url": "/react-project2/tree/master/react-app/static/js/2.29e3f1dc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-project2/tree/master/react-app/static/js/2.29e3f1dc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5763d792ba47c2594e5",
+    "url": "/react-project2/tree/master/react-app/static/js/main.95842cb2.chunk.js"
+  },
+  {
+    "revision": "fdd05abc0c683433aa6f",
+    "url": "/react-project2/tree/master/react-app/static/js/runtime-main.ed15923b.js"
+  }
+]);
